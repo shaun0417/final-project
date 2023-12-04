f978b792d012dabae38125adc544f07103beb9b4
shaun0417 <150244502+shaun0417@users.noreply.github.com>
2023-12-05 02:18:49 +0900

    Add files via upload

---
+++ raining cats and fish.py
@@ -0,0 +1,208 @@
+
+import pygame
+import random
+from os import path
+
+img_dir = path.join(path.dirname(__file__), 'img')
+snd_dir = path.join(path.dirname(__file__), 'snd')
+
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+# initialize pygame and create window
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("raining cats and fish!!")
+clock = pygame.time.Clock()
+
+font_name = pygame.font.match_font('arial')
+def draw_text(surf, text, size, x, y):
+    font = pygame.font.Font(font_name, size)
+    text_surface = font.render(text, True, WHITE)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    surf.blit(text_surface, text_rect)
+
+def newmob():
+    m = Mob()
+    all_sprites.add(m)
+    mobs.add(m)
+
+def newbad():
+    b = BadFish()
+    all_sprites.add(b)
+    badfish.add(b)
+    
+def draw_lives(surf, x, y, lives, img):
+    for i in range(lives):
+        img_rect = img.get_rect()
+        img_rect.x = x + 30 * i
+        img_rect.y = y
+        surf.blit(img, img_rect)
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(player_img, (90, 70))
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.radius = 50
+        # pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+        self.lives = 1
+        self.hidden = False
+        self.hide_timer = pygame.time.get_ticks()
+
+    def update(self):
+        # unhide if hidden
+        if self.hidden and pygame.time.get_ticks() - self.hide_timer > 1000:
+            self.hidden = False
+            self.rect.centerx = WIDTH / 2
+            self.rect.bottom = HEIGHT - 10
+
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -8
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 8
+        self.rect.x += self.speedx
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+    def hide(self):
+        # hide the player temporarily
+        self.hidden = True
+        self.hide_timer = pygame.time.get_ticks()
+        self.rect.center = (WIDTH / 2, HEIGHT + 200)
+
+class Mob(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image_orig = random.choice(fish_images_small)
+        self.image_orig.set_colorkey(BLACK)
+        self.image = self.image_orig.copy()
+        self.rect = self.image.get_rect()
+        self.radius = int(self.rect.width * .85 / 2)
+        # pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.bottom = random.randrange(-80, -20)
+        self.speedy = random.randrange(5, 10)
+        self.last_update = pygame.time.get_ticks()
+
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT + 10:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(5, 8)
+
+class BadFish(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image_orig = bad_fish
+        self.image_orig.set_colorkey(BLACK)
+        self.image = self.image_orig.copy()
+        self.rect = self.image.get_rect()
+        self.radius = int(self.rect.width * .85 / 2)
+        # pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.bottom = random.randrange(-80, -20)
+        self.speedy = random.randrange(5, 10)
+        self.last_update = pygame.time.get_ticks()
+
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT + 10:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(5, 8)
+
+
+# Load all game graphics
+background = pygame.image.load(path.join(img_dir, "parallax-space-backgound.png")).convert()
+background_rect = background.get_rect()
+player_img = pygame.image.load(path.join(img_dir, "cat.png")).convert()
+player_mini_img = pygame.transform.scale(player_img, (25, 19))
+player_mini_img.set_colorkey(BLACK)
+fish_images = []
+fish_images_small=[]
+fish_list = ['fish-blue.png','fish-green.png','fish-purple.png','fish-yellow.png']
+for img in fish_list:
+    fish_images.append(pygame.image.load(path.join(img_dir, img)).convert())
+for img in fish_images:
+    fish_images_small.append(pygame.transform.scale(img,(40,60)))
+bad_fish_big= pygame.image.load(path.join(img_dir,'fish-poison.png')).convert()
+bad_fish=pygame.transform.scale(bad_fish_big,(50,60))
+
+# Load all game sounds
+eat_sounds = pygame.mixer.Sound(path.join(snd_dir,'eat.wav'))
+player_die_sound = pygame.mixer.Sound(path.join(snd_dir, 'explosion04.wav'))
+pygame.mixer.music.load(path.join(snd_dir, 'HappyTown.mp3'))
+pygame.mixer.music.set_volume(0.4)
+
+all_sprites = pygame.sprite.Group()
+mobs = pygame.sprite.Group()
+badfish = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+for i in range(7):
+    newmob()
+for i in range(2):
+    newbad()
+
+
+score = 0
+pygame.mixer.music.play(loops=-1)
+# Game loop
+running = True
+while running:
+    # keep loop running at the right speed
+    clock.tick(FPS)
+    # Process input (events)
+    for event in pygame.event.get():
+        # check for closing window
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Update
+    all_sprites.update()
+
+    # check to see if a mob hit the player
+    hits = pygame.sprite.spritecollide(player, mobs, True, pygame.sprite.collide_circle)
+    for hit in hits:
+        score += 30 - hit.radius
+        eat_sounds.play()
+        newmob()
+
+    # check to see if a badfish hit the player
+    hits = pygame.sprite.spritecollide(player, badfish, True, pygame.sprite.collide_circle)
+    for hit in hits:
+        player.lives -= 1
+        if player.lives == 0 :
+            running = False
+        newmob()
+
+    # Draw / render
+    screen.fill(BLACK)
+    screen.blit(background, background_rect)
+    all_sprites.draw(screen)
+    draw_text(screen, str(score), 18, WIDTH / 2, 10)
+    # *after* drawing everything, flip the display
+    pygame.display.flip()
+
+pygame.quit()
